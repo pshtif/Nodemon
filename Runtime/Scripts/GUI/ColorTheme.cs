1268d4ac0f939d85c9b6f100e926664a2edece89
Peter @sHTiF Stefcek <pshtif@gmail.com>
2023-04-20 21:12:35 +0200

    add colortheme static will be moved to theme management later

---
+++ Runtime/Scripts/GUI/ColorTheme.cs
@@ -0,0 +1,13 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using UnityEngine;
+
+namespace Machina
+{
+    public static class ColorTheme
+    {
+        public static Color BUTTON_COLOR = new Color(1, 0.75f, 0.5f);
+    }
+}
