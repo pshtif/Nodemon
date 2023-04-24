caa14ae1605d00a4bc653230ba29d6b67cad6683
Peter @sHTiF Stefcek <pshtif@gmail.com>
2023-04-24 09:44:40 +0200

    encampsulated attributes

--- Runtime/Scripts/Graph/Node/DataAttribute.cs
+++ Runtime/Scripts/Graph/Node/DataAttribute.cs
@@ -6,6 +6,30 @@ namespace Nodemon
 {
     public class DataAttribute
     {
-        
+        public string name = "";
+        public object value;
+
+        public virtual DataAttribute Clone()
+        {
+            var clone = CreateClonedInstance();
+            clone.name = name;
+            
+            if (value.GetType().IsValueType)
+            {
+                clone.value = value;
+            }
+            
+            if (value.GetType() == typeof(int[]))
+            {
+                clone.value = ((int[])value).Clone();
+            }
+
+            return clone;
+        }
+
+        protected virtual DataAttribute CreateClonedInstance()
+        {
+            return new DataAttribute();
+        }
     }
 }
--- Runtime/Scripts/Graph/Node/NodeFlowData.cs
+++ Runtime/Scripts/Graph/Node/NodeFlowData.cs
@@ -5,24 +5,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Nodemon
 {
-    public class NodeFlowData : IAttributeDataCollection, IEnumerable
+    public class NodeFlowData<T> : IAttributeDataCollection<T>, IEnumerable where T : DataAttribute
     {
-        static readonly public NodeFlowData[] EMPTY_ARRAY = {};
-        static readonly public NodeFlowData EMPTY = new NodeFlowData();
+        static readonly public NodeFlowData<T>[] EMPTY_ARRAY = {};
+        static readonly public NodeFlowData<T> EMPTY = new NodeFlowData<T>();
 
-        protected Dictionary<string, object> _attributes;
+        protected Dictionary<string, T> _attributes;
 
         public NodeFlowData()
         {
-            _attributes = new Dictionary<string, object>();
+            _attributes = new Dictionary<string, T>();
         }
-        
-        public void BindFromDictionary(Dictionary<string, object> p_properties)
+
+        public void BindFromDictionary(Dictionary<string, T> p_properties)
         {
-            _attributes = new Dictionary<string, object>(p_properties);
+            _attributes = new Dictionary<string, T>(p_properties);
         }
 
         public bool HasAttribute(string p_name)
@@ -34,7 +35,7 @@ namespace Nodemon
         {
             if (HasAttribute(p_name))
             {
-                return _attributes[p_name].GetType();
+                return _attributes[p_name].value.GetType();
             }
 
             return null;
@@ -45,42 +46,45 @@ namespace Nodemon
             _attributes.Remove(p_name);
         }
 
-        public T GetAttribute<T>(string p_name)
+        public K GetAttributeValue<K>(string p_name)
         {
             if (HasAttribute(p_name))
             {
-                return (T) _attributes[p_name];
+                return (K)_attributes[p_name].value;
             }
 
-            return default(T);
+            return default(K);
+        }
+        
+        public object GetAttributeValue(string p_name)
+        {
+            if (HasAttribute(p_name))
+            {
+                return _attributes[p_name].value;
+            }
+
+            return null;
         }
 
-        public object GetAttribute(string p_name)
+        public T GetAttribute(string p_name)
         {
             return _attributes.ContainsKey(p_name) ? _attributes[p_name] : null;
         }
 
-        public void SetAttribute(string p_name, object p_value)
+        public void SetAttribute(T p_attribute)
         {
-            if (_attributes.ContainsKey(p_name))
-            {
-                _attributes[p_name] = p_value;
-            }
-            else
-            {
-                _attributes.Add(p_name, p_value);
-            }
+            _attributes[p_attribute.name] = p_attribute;
         }
 
-        public NodeFlowData Clone()
+        public virtual NodeFlowData<T> Clone()
         {
-            var ndf = new NodeFlowData();
-            ndf.BindFromDictionary(CloneAttributes(_attributes));
+            var nfd = new NodeFlowData<T>();
+            nfd.BindFromDictionary(CloneAttributes(_attributes));
             
-            return ndf;
+            return nfd;
         } 
         
-        public Dictionary<string, object>.Enumerator GetEnumerator()
+        public Dictionary<string, T>.Enumerator GetEnumerator()
         {
             return _attributes.GetEnumerator();
         }
@@ -90,29 +94,24 @@ namespace Nodemon
             return ((IEnumerable)_attributes.Values).GetEnumerator();
         }
         
-        public static Dictionary<string, object> CloneAttributes(Dictionary<string, object> p_original) 
+        public List<T> GetNameSortedAttributes()
         {
-            Dictionary<string, object> clone = new Dictionary<string, object>(p_original.Count, p_original.Comparer);
-            foreach (KeyValuePair<string, object> pair in p_original)
+            var sortedList = new List<T>(_attributes.Values);
+            sortedList.Sort(delegate(T p_attribute1, T p_attribute2)
             {
-                if (pair.Value == null || pair.Value.GetType().IsValueType)
-                {
-                    clone.Add(pair.Key, pair.Value);
-                    continue;
-                }
-
-                if (pair.Value.GetType() == typeof(int[]))
-                {
-                    clone.Add(pair.Key, ((int[])pair.Value).Clone());
-                    continue;           
-                }
-                
-                // TODO deep cloning for other types
-                clone.Add(pair.Key, pair.Value);
+                return p_attribute1.name.CompareTo(p_attribute2.name);
+            });
+            return sortedList;
+        }
+        
+        public static Dictionary<string, T> CloneAttributes(Dictionary<string, T> p_original) 
+        {
+            Dictionary<string, T> clone = new Dictionary<string, T>(p_original.Count, p_original.Comparer);
+            foreach (KeyValuePair<string, T> pair in p_original)
+            {
+                clone.Add(pair.Key, (T)pair.Value.Clone());
             }
             return clone;
         }
     }
-    
-    // TODO implement pooling
 }
