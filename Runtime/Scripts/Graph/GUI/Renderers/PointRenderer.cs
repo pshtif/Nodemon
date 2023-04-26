776c0b385d3260987237977fee629f14e4e957be
Peter @sHTiF Stefcek <pshtif@gmail.com>
2023-04-26 09:28:38 +0200

    more point render experiments

--- Runtime/Scripts/Graph/GUI/Renderers/PointRenderer.cs
+++ Runtime/Scripts/Graph/GUI/Renderers/PointRenderer.cs
@@ -11,11 +11,18 @@ namespace Nodemon
     public class PointRenderer
     {
         private static Material _pointMaterial;
-        private static Texture2D _pointTexture;
 
         private static bool _immediateMode;
 
-        private static void SetupPointMaterial(Color p_color, Texture p_texture) 
+        public static Material GetPointMaterial(Color p_color)
+        {
+            InitializePointMaterial();
+            
+            _pointMaterial.SetColor("_PointColor", p_color);
+            return _pointMaterial;
+        }
+
+        private static void InitializePointMaterial()
         {
             if (_pointMaterial == null)
             {
@@ -26,24 +33,22 @@ namespace Nodemon
                 }
 
                 _pointMaterial = new Material(lineShader);
+                _pointMaterial.SetTexture("_PointTexture", TextureUtils.GetTexture("point") as Texture2D);
             }
+        }
+        
+        private static void SetPointMaterialPass(Color p_color)
+        {
+            InitializePointMaterial();
             
-            if (p_texture == null)
-            {
-                p_texture = _pointTexture != null
-                    ? _pointTexture
-                    : _pointTexture = TextureUtils.GetTexture("point") as Texture2D;
-            }
-
-            _pointMaterial.SetTexture("_PointTexture", p_texture);
             _pointMaterial.SetColor("_PointColor", p_color);
             _pointMaterial.SetPass(0);
         }
 
-        public static void BeginImmediateMode(Color p_color, Texture2D p_texture = null)
+        public static void BeginImmediateMode(Color p_color)
         {
             _immediateMode = true;
-            SetupPointMaterial(p_color, p_texture);
+            SetPointMaterialPass(p_color);
             GL.Begin(GL.QUADS);
             GL.Color(Color.white);
         }
@@ -54,11 +59,11 @@ namespace Nodemon
             GL.End();
         }
         
-        public static void DrawPoint(Vector3 p_position, float p_size, Camera p_camera, Color p_color, Texture2D p_texture = null)
+        public static void DrawPoint(Vector3 p_position, float p_size, Camera p_camera, Color p_color)
         {
             if (!_immediateMode)
             {
-                SetupPointMaterial(p_color, p_texture);
+                SetPointMaterialPass(p_color);
                 GL.Begin(GL.TRIANGLES);
                 GL.Color(Color.white);
             }
